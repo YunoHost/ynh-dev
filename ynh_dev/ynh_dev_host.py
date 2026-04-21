4174c7a407b9580cc782fe610b8f6d76e74b3e73
Félix Piédallu <felix@piedallu.me>
2026-04-21 22:43:27 +0200

    Fix ensure_incus_remote

--- ynh_dev/ynh_dev_host.py
+++ ynh_dev/ynh_dev_host.py
@@ -115,9 +115,9 @@ def check_incus_setup() -> None:
 
 
 def ensure_incus_remote() -> None:
-    remote_url = "https://repo.yunohost.org/incus/"
+    remote_url = "https://repo.yunohost.org/incus"
     if ynh_remote := Incus().remotes().get("yunohost"):
-        if (url := ynh_remote["Addr"]) != remote_url:
+        if (url := ynh_remote["Addr"].removesuffix("/")) != remote_url:
             logging.error(f"Remote yunohost has url {url} instead of {remote_url}!")
             sys.exit(1)
         return
